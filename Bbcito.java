461ab41b75bd3238c11d68156bba6f6e0f847e82
Guillermo Trejo <79237995+GuillermoTrejo07@users.noreply.github.com>
2021-02-17 21:03:30 -0600

    Practica 1
    My first learning!

---
+++ Bbcito.java
@@ -0,0 +1,23 @@
+/**
+ * Auto Generated Java Class.
+ */
+public class Bbcito {
+  
+  /* ADD YOUR CODE HERE */
+  public static void main (String[] args) {
+    //System.out.println(" abadacadabra ");
+    int first=7;
+    int kul=2;
+    kul=kul+first;
+    System.out.println (kul);
+    int moi=Bbcito.plus (7,2);
+    System.out.println (moi);
+    
+    
+    }  
+  public static int plus (int x,int y) {
+    int result=x+y;
+    return result;
+    
+  }   
+}
